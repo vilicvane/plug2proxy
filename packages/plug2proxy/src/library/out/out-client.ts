9f46746cc27120a6c318c8194daf4ab3b24e8dcc
vilicvane <vilicvane@live.com>
2021-11-29 10:40:38 +0800

    Clean up idle connection

--- packages/plug2proxy/src/library/out/out-client.ts
+++ packages/plug2proxy/src/library/out/out-client.ts
@@ -6,7 +6,11 @@ import {Router} from '../router';
 import {OutInConnection} from './out-in-connection';
 
 const RETRIEVED_AT_WINDOW = 2_000;
-const IDLE_CONNECTION_SCHEDULE_DELAY = 1_000;
+
+const MAX_IDLE_DURATION = 2 * 60_000;
+
+const IDLE_CONNECTION_SCALING_SCHEDULE_DELAY = 1_000;
+const IDLE_CONNECTION_CLEAN_UP_SCHEDULE_DELAY = 10_000;
 
 const INITIAL_CONNECTIONS_DEFAULT = 2;
 const SCALE_MULTIPLIER_DEFAULT = 1;
@@ -26,6 +30,7 @@ export class OutClient {
   private retrievedAts: number[] = [];
 
   private idleConnectionScalingTimeout: NodeJS.Timeout | undefined;
+  private idleConnectionCleanUpTimeout: NodeJS.Timeout | undefined;
 
   private initialConnections: number;
   private scaleMultiplier: number;
@@ -42,6 +47,15 @@ export class OutClient {
     this.scaleMultiplier = scaleMultiplier;
 
     this.createIdleConnections(initial);
+
+    setInterval(() => {
+      let now = Date.now();
+
+      for (let connection of this.idleConnectionSet) {
+        if (now - connection.idledAt > MAX_IDLE_DURATION) {
+        }
+      }
+    }, MAX_IDLE_DURATION / 2);
   }
 
   retrieveIdleConnection(connection: OutInConnection): OutInConnection {
@@ -61,7 +75,11 @@ export class OutClient {
 
     this.scaleIdleConnections();
 
-    debug('retrieved idle connection to %s', connection.remoteAddress);
+    debug(
+      'retrieved idle connection to %s, now %d idle in total',
+      connection.remoteAddress,
+      idleConnectionSet.size,
+    );
 
     return connection;
   }
@@ -73,7 +91,11 @@ export class OutClient {
 
     this.scheduleIdleConnectionScaling();
 
-    debug('removed idle connection to %s', connection.remoteAddress);
+    debug(
+      'removed idle connection to %s, now %d idle in total',
+      connection.remoteAddress,
+      idleConnectionSet.size,
+    );
   }
 
   addIdleConnection(connection: OutInConnection): void {
@@ -81,7 +103,11 @@ export class OutClient {
 
     idleConnectionSet.add(connection);
 
-    debug('added idle connection to %s', connection.remoteAddress);
+    debug(
+      'added idle connection to %s, now %d idle in total',
+      connection.remoteAddress,
+      idleConnectionSet.size,
+    );
   }
 
   returnIdleConnection(connection: OutInConnection): void {
@@ -91,7 +117,11 @@ export class OutClient {
 
     idleConnectionSet.add(connection);
 
-    debug('returned idle connection to %s', connection.remoteAddress);
+    debug(
+      'returned idle connection to %s, now %d idle in total',
+      connection.remoteAddress,
+      idleConnectionSet.size,
+    );
   }
 
   private scaleIdleConnections(): void {
@@ -117,6 +147,8 @@ export class OutClient {
         idleConnectionSet.size,
       );
     }
+
+    this.cleanUpIdleConnections();
   }
 
   private scheduleIdleConnectionScaling(): void {
@@ -126,20 +158,34 @@ export class OutClient {
 
     this.idleConnectionScalingTimeout = setTimeout(() => {
       this.scaleIdleConnections();
-    }, IDLE_CONNECTION_SCHEDULE_DELAY);
+    }, IDLE_CONNECTION_SCALING_SCHEDULE_DELAY);
+  }
+
+  private cleanUpIdleConnections(): void {
+    let now = Date.now();
+
+    for (let connection of this.idleConnectionSet) {
+      if (now - connection.idledAt > MAX_IDLE_DURATION) {
+        connection.socket.destroy();
+      }
+    }
+
+    this.scheduleIdleConnectionCleanUp();
+  }
+
+  private scheduleIdleConnectionCleanUp(): void {
+    if (this.idleConnectionCleanUpTimeout) {
+      clearTimeout(this.idleConnectionCleanUpTimeout);
+    }
+
+    this.idleConnectionCleanUpTimeout = setTimeout(() => {
+      this.cleanUpIdleConnections();
+    }, IDLE_CONNECTION_CLEAN_UP_SCHEDULE_DELAY);
   }
 
   private createIdleConnections(count: number): void {
-    let idleConnectionSet = this.idleConnectionSet;
-
     for (let i = 0; i < count; i++) {
       new OutInConnection(this);
     }
-
-    debug(
-      'created %d connection(s), now %d idle in total',
-      count,
-      idleConnectionSet.size,
-    );
   }
 }
