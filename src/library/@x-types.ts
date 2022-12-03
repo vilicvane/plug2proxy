b8a0b3a93410fe0a810745468001468297057f6a
vilicvane <vilicvane@live.com>
2022-12-04 07:26:06 +0800

    Fix x-value version and usage

--- src/library/@x-types.ts
+++ src/library/@x-types.ts
@@ -3,14 +3,17 @@ import * as x from 'x-value';
 
 export const Port = x.integerRange<'port'>({min: 1, max: 65535});
 
-export const IPMatchPattern = x.string.refine<'ip match pattern'>(value => {
+export const IPMatchPattern = x.string.refined<'ip match pattern'>(value => {
   IPMatching.getMatch(value);
+  return value;
 });
 
-export const IPPattern = x.string.refine<'ip pattern'>(value => {
+export const IPPattern = x.string.refined<'ip pattern'>(value => {
   let ip = IPMatching.getIP(value);
 
   if (ip === null) {
     throw new TypeError('Invalid IP address');
   }
+
+  return value;
 });
