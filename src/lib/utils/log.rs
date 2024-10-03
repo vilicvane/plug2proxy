d6b602825fcfb7a09f499de2fe5aa1214a1de8d9
vilicvane <vilicvane@live.com>
2024-10-03 13:41:26 +0800

    refine default log filter

--- src/lib/utils/log.rs
+++ src/lib/utils/log.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::{env, io::Write};
 
 use colored::Colorize;
 use env_logger::Env;
@@ -7,21 +7,23 @@ use log::Level;
 pub fn init_log() {
     color_backtrace::install();
 
-    env_logger::Builder::from_env(Env::default().default_filter_or("debug"))
-        .format(|f, record| {
-            let timestamp = chrono::Local::now()
-                .format("%Y-%m-%d %H:%M:%S.%3f")
-                .to_string();
+    env_logger::Builder::from_env(
+        Env::default().default_filter_or(format!("{}=info", env!("CARGO_PKG_NAME"))),
+    )
+    .format(|f, record| {
+        let timestamp = chrono::Local::now()
+            .format("%Y-%m-%d %H:%M:%S.%3f")
+            .to_string();
 
-            let timestamp = match record.level() {
-                Level::Error => timestamp.bright_red(),
-                Level::Warn => timestamp.bright_yellow(),
-                Level::Info => timestamp.bright_green(),
-                Level::Debug => timestamp.bright_black(),
-                Level::Trace => timestamp.dimmed(),
-            };
+        let timestamp = match record.level() {
+            Level::Error => timestamp.bright_red(),
+            Level::Warn => timestamp.bright_yellow(),
+            Level::Info => timestamp.bright_green(),
+            Level::Debug => timestamp.bright_black(),
+            Level::Trace => timestamp.dimmed(),
+        };
 
-            writeln!(f, "{} {}", timestamp, record.args())
-        })
-        .init();
+        writeln!(f, "{} {}", timestamp, record.args())
+    })
+    .init();
 }
