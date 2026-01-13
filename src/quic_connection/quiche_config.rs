13ae6f645536088378a2d45c30674f83f66c71c4
vilicvane <id@wsh.earth>
2026-01-14 02:33:38 +0800

    wip: tweak quic config

--- src/quic_connection/quiche_config.rs
+++ src/quic_connection/quiche_config.rs
@@ -8,7 +8,7 @@ use lits::{bytes, duration};
 
 pub const MAX_DATAGRAM_SIZE: usize = bytes!("64 KiB") as usize;
 
-pub const MAX_DATA_BUFFER_SIZE: u64 = bytes!("32 MiB");
+pub const MAX_DATA_BUFFER_SIZE: u64 = bytes!("64 MiB");
 
 pub static UNSPECIFIED_SOCKET_ADDRESS: LazyLock<SocketAddr> =
   LazyLock::new(|| SocketAddr::from((IpAddr::V4(Ipv4Addr::UNSPECIFIED), 0)));
@@ -39,9 +39,6 @@ pub fn create_quiche_config(pem_path: impl AsRef<Path>) -> quiche::Result<quiche
   config.set_initial_max_streams_bidi(1024);
   config.set_initial_max_streams_uni(1024);
   config.set_disable_active_migration(true);
-
-  // Use BBR congestion control since we're running over TCP
-  config.set_cc_algorithm(quiche::CongestionControlAlgorithm::Bbr2Gcongestion);
 
   config.load_cert_chain_from_pem_file(pem_path)?;
   config.load_priv_key_from_pem_file(pem_path)?;
